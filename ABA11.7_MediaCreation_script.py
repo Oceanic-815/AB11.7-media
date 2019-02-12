e85fe5a5a9361cdf632657c02cb641e553477a72
Alexander Krasnov <rn4acy@mail.ru>
2019-02-12 21:23:30 +0300

    Handling of incorrect data input changed; Added exceptions handling
    Handling of incorrect data input was improved;
    Added exceptions handling.

--- ABA11.7_MediaCreation_script.py
+++ ABA11.7_MediaCreation_script.py
@@ -17,20 +17,24 @@ How To Use it:
 """
 
 from pywinauto.application import Application
+from pywinauto import application
+from pywinauto import base_wrapper
+from pywinauto import MatchError
+from pywinauto import ElementNotFoundError
 import os
 import time
 import logging
 import json
 
 
-build_number = "50220"  # Specify a build number with "_" character in the end. Example: "50064_"
-if_na = True            # Set True if NA build is used
+build_number = "50230"  # Specify a build number with "_" character in the end. Example: "50064_"
+if_na = False            # Set True if NA build is used
 
 
 try:
     logging.basicConfig(level=logging.INFO, filename='C:\MediaCreationLog.log', format='%(asctime)s %(message)s')
 except:
-    pass
+    print("Error occurred during log file creation")
 
 installer_folder_list = []  # Folders list
 get_curr_dir = os.getcwd()
@@ -50,10 +54,10 @@ else:
 
 list_of_all_localization_symbols = []  # List that contains all symbols related to localization part of folder names
 iteration = 0
-list_of_grouped_localizations = []  # List of lists of localizations
+list_of_grouped_localizations = []  # A list of lists of localizations
 localization_list = []  # Ready localizations used for main script
 local = ''
-for i in installer_folder_list:  # Extending list_of_all_localization_symbols list
+for i in installer_folder_list:  # Extending list_of_all_localization_symbols list, e.g. [en-US, fr-FR]
     if if_na:
         a = i[34:]
     else:
@@ -66,12 +70,12 @@ for i in installer_folder_list:  # Extending list_of_all_localization_symbols li
                 len(list_of_grouped_localizations)):  # Finalizing localization list
             local = ''.join(list_of_grouped_localizations[new_cycle_to_get_local_list])
         localization_list.extend([local])
-print(localization_list)
+print("Following localizations available: " + str(localization_list))
 logging.info("Installers with the following localizations are found in the folder: " + str(localization_list))
 
 
 try:
-    data = json.load(open(".\\keys.json", 'r'))  # Opening JSON with licenses
+    data = json.load(open(".\\keys.json", 'r'))  # Opening JSON with keys
     keys_list = data["main_keys"]
 except FileNotFoundError:
     print("ERROR: json file with keys not found! Put the file into the root folder along with the script.")
@@ -87,15 +91,15 @@ except FileNotFoundError:
 names_list = ["AcronisBackupAdvancedWS_11.7_", "AcronisBackupAdvancedUniversal_11.7_", "AcronisBackupAdvancedHyperV_11.7_", "AcronisBackupAdvancedVMware_11.7_", "AcronisBackupAdvancedRHEV_11.7_", "AcronisBackupAdvancedXEN_11.7_", "AcronisBackupAdvancedOracle_11.7_", "AcronisBackupEssentials_11.7_", "AcronisBackupAdvancedPC_11.7_", "AcronisBackupWS_11.7_", "AcronisBackupPC_11.7_"]
 na_names_list = ['AcronisBackupAdvancedWS_11.7N_', 'AcronisBackupAdvancedUniversal_11.7N_', 'AcronisBackupAdvancedHyperV_11.7N_', 'AcronisBackupAdvancedVMware_11.7N_', 'AcronisBackupAdvancedRHEV_11.7N_', 'AcronisBackupAdvancedXEN_11.7N_', 'AcronisBackupAdvancedOracle_11.7N_', 'AcronisBackupEssentials_11.7N_', 'AcronisBackupAdvancedPC_11.7N_', 'AcronisBackupWS_11.7N_', 'AcronisBackupPC_11.7N_']
 
-os.chdir(".\\media")
-current_working_directory = os.getcwd()
+os.chdir(".\\media")  # Change current working directory to dir for ISOs
+current_working_directory = os.getcwd()  # Getting the current work dir to use it in Installation func.
 
 try:
     for i in range(len(localization_list)):  # Creating subfolders in /media folder for ISO
         os.makedirs(localization_list[i])
 except FileExistsError:
-    print("Target folders exist")
-    logging.info("Target folders exist")
+    print("Target sub-folders exist")
+    logging.info("Target sub-folders exist")
 
 
 def uninstallation():
@@ -124,7 +128,12 @@ def installation(current_local):
     else:
         installation_command = 'start /wait msiexec /i ' + parent_of_current_dir + '\\installers\\AcronisBackupAdvanced_11.7_' + build_number + '_' + current_local + '\\AcronisBootableComponentsMediaBuilder.msi /quiet /qn'
     print(installation_command)
-    os.system(installation_command)
+    try:
+        os.system(installation_command)
+    except application.AppStartError:
+        print("Check if build number is correct. If NA build is used, specify 'if_na = True', else 'if_na = False'")
+        logging.info("Check if build number is correct. If NA build is used, specify 'if_na = True', else 'if_na = False'")
+        exit()
     if os.path.exists('C:\\Program Files (x86)\\Common Files\\Acronis\\MediaBuilder\\MediaBuilder.exe'):
         print("Installation complete!")
         logging.info("Installation complete!")
@@ -138,60 +147,70 @@ def main_script(key_list_f, names_list_f, locale):
     for k in range(len(key_list_f)):  # k is an index of a license. This is a loop of creating ISOs
         logging.info("ISO creation starts...")
         new_iso_name = current_working_directory + "\\" + locale + "\\" + names_list_f[k] + build_number + '_' + locale
-        app = Application().start("C:\Program Files (x86)\Common Files\Acronis\MediaBuilder\MediaBuilder.exe")
+        app = Application()
+        try:
+            app.start("C:\Program Files (x86)\Common Files\Acronis\MediaBuilder\MediaBuilder.exe")
+        except application.AppStartError:
+            print("Check if build number is correct. If NA build is used, specify 'if_na = True', else 'if_na = False'")
+            logging.info("Check if build № is correct. If NA build installed, specify 'if_na = True', else 'if_na = False'")
+            exit()
         time.sleep(2)
         window = app.window_()
         window.SetFocus()
         buildwizard = app.BuilderWizard
         buildwizard.Wait('ready')
         next_button = buildwizard[u'Next >FXButton3']
+        logging.info("Clicking 'Next' button in the start Wizard page")
         next_button.click()
-        logging.info("'Next' button in the start Wizard page was clicked")
         next_button.wait('ready', timeout=20)
+        logging.info("Clicking 'Next' button in the 'Select media type' page")
         next_button.click()
-        logging.info("'Next' button in the 'Select media type' page was clicked")
         next_button.wait('ready', timeout=20)
+        logging.info("Clicking 'Next' button in the 'Kernel parameters' page")
         next_button.click()
-        logging.info("'Next' button in the 'Kernel parameters' page was clicked")
         buildwizard.FXAConfigurationTree1.click()
+        logging.info("Selecting AB x86/x64 components check boxes")
         buildwizard.send_keystrokes('{SPACE}')
+        logging.info("Clicking 'Next' button in the 'Components' page")
         next_button.click()
-        logging.info("'Next' button in the 'Components' page was clicked")
         next_button.wait('ready', timeout=20)
+        logging.info("Clicking 'Next' button in the 'Network and remote connection' page")
         next_button.click()
-        logging.info("'Next' button in the 'Network and remote connection' page was clicked")
+        logging.info("Clicking the first 'YES' button")
         buildwizard.send_keystrokes('{ENTER}')
-        logging.info("The first 'YES' button was clicked")
+        logging.info("Clicking the second 'YES' button")
         buildwizard.send_keystrokes('{ENTER}')
-        logging.info("The second 'YES' button was clicked")
+        logging.info("Selecting a radio button to enter a key")
         buildwizard.send_keystrokes('{DOWN}')
         buildwizard.send_keystrokes('{DOWN}')
         buildwizard.send_keystrokes('{SPACE}')
+        logging.info("Selecting Text field for entering a key")
         fxtext = buildwizard['FXText']
         fxtext.click()
-        logging.info("Selecting Text field for entering a key")
         fxtext.send_chars(key_list_f[k])
         fxtext.click()
-        key_copy = fxtext.WindowText()
-        if key_copy.upper() != key_list_f[k].upper():  # Check if entered key equals to original key
-            logging.info("Wrong key is specified: " + str(key_copy) + "\nRetyping key...")
-            print("Wrong key is specified: " + str(key_copy))
-            fxtext.click()
-            fxtext.send_keystrokes('^a')
-            fxtext.send_keystrokes('{DELETE}')
-            fxtext.send_chars(key_list_f[k])
-        else:
-            print("Specified license key is correct.")
-            logging.info("Specified license key is correct.")
-        logging.info(str(key_list_f[k]) + " is selected for media")
-        time.sleep(1)
+        while True:  # Re-type key if it is not correct
+            key_copy = fxtext.WindowText()
+            if key_copy.upper() != key_list_f[k].upper():
+                print("Entered key is NOT correct. Re-typing...")
+                logging.info("Entered key is NOT correct. Re-typing...")
+                fxtext.click()
+                fxtext.send_keystrokes('^a')
+                fxtext.send_keystrokes('{DELETE}')
+                fxtext.send_chars(key_list_f[k])
+            else:
+                print("Entered key is correct. Continue...")
+                logging.info("Entered key is correct. Continue...")
+                break
+        time.sleep(0)
         window.SetFocus()
         fxtext.wait('ready', timeout=20)
+        logging.info("Clicking 'Next' button in the 'Enter a key' page")
         next_button.click()
-        logging.info("'Next' button in the 'Enter a license key' page was clicked")
+        logging.info("Clicking 'Next' button in the 'Specified key' page")
         next_button.wait('ready', timeout=20)
         next_button.click()
-        logging.info("'Next' button in the 'Specified license' page was clicked")
+        logging.info("Selecting a panel with a list of targets")
         buildwizard.send_keystrokes('{TAB}')
         # ***SELECT "ISO" AS A MEDIA TYPE:*** (Here we select ISO item from a media type list according to localization)
         if locale == "en-US" or locale == "en-EU" or locale == "de-DE" or locale == "zh-TW" or locale == "ja-JP" or locale == "ko-KR" or locale == "pl-PL" or locale == "zh-CN" or locale == "es-ES":
@@ -208,45 +227,48 @@ def main_script(key_list_f, names_list_f, locale):
             buildwizard.send_keystrokes('{UP}')
         time.sleep(1)
         next_button.SetFocus()
+        logging.info("Clicking 'Next' button in the 'Bootable Media format' page")
         next_button.click()
-        logging.info("'Next' button in the 'Bootable Media format' page was clicked")
         time.sleep(1)
+        logging.info("Clicking a path field")
         buildwizard.FXAFileNameField.click()
         buildwizard.FXAFileNameField.send_keystrokes('^a')
         buildwizard.FXAFileNameField.send_keystrokes('{DELETE}')
         logging.info("ISO name field cleaned up")
-        time.sleep(1)
+        time.sleep(0)
         buildwizard.FXAFileNameField.send_chars(new_iso_name)
-        iso_name__copy = buildwizard.FXAFileNameField.WindowText()
-        if iso_name__copy.lower() != new_iso_name.lower():  # Checking if ISO name was correctly entered
-            buildwizard.FXAFileNameField.click()
-            buildwizard.FXAFileNameField.send_keystrokes('^a')
-            buildwizard.FXAFileNameField.send_keystrokes('{DELETE}')
-            time.sleep(1)
-            logging.info("Re-typing ISO name...")
-            buildwizard.FXAFileNameField.send_chars(new_iso_name)
-            print("ISO name is correct!")
-            logging.info("'" + new_iso_name + "' is a correct ISO name")
-        else:
-            print("ISO name is correct!")
-            logging.info("'" + new_iso_name + "' is a correct ISO name")
-        time.sleep(1)
+        while True:  # Re-type ISO name if it is not correct
+            iso_name__copy = buildwizard.FXAFileNameField.WindowText()
+            if iso_name__copy.lower() != new_iso_name.lower():  # Checking if ISO name was correctly entered
+                buildwizard.FXAFileNameField.click()
+                buildwizard.FXAFileNameField.send_keystrokes('^a')
+                buildwizard.FXAFileNameField.send_keystrokes('{DELETE}')
+                time.sleep(0)
+                logging.info("Re-typing ISO name...")
+                buildwizard.FXAFileNameField.send_chars(new_iso_name)
+                print("ISO name is NOT correct! Re-typing...")
+                logging.info("'" + new_iso_name + "' is NOT a correct ISO name. Re-typing")
+            else:
+                print("ISO name is correct! Continue...")
+                logging.info("'" + new_iso_name + "' is a correct ISO name. Continue...")
+                break
+        time.sleep(0)
         next_button.SetFocus()
         next_button.wait('exists visible enabled ready active', timeout=20)
         next_button.set_focus()
+        logging.info("Clicking 'Next' button in the 'Specify name for ISO' page")
         next_button.click()
-        logging.info("'Next' button in the 'Specify name for ISO' page was clicked")
+        logging.info("Clicking 'Next' button in the 'Add drivers' page")
         next_button.click()
-        logging.info("'Next' button in the 'Add drivers' page was clicked")
+        logging.info("Clicking 'Proceed' button in the 'SUMMARY' page")
         next_button.wait('ready')
         next_button.set_focus()
         next_button.click()
-        logging.info("'Proceed' button in the 'SUMMARY' was clicked")
         finish_message_box = app.FXAMessageBoxImpl
         finish_message_box.wait("ready active", timeout=50)
         ok_button_in_box = finish_message_box.FXButton
+        logging.info("Clicking 'OK' button in the message box")
         ok_button_in_box.click()
-        logging.info("'OK' button in the message box was clicked")
         if os.path.exists(new_iso_name + ".iso"):  # ISO creation check
             print('Media is created')
             logging.info('Media is created \n====================')
@@ -260,16 +282,27 @@ def start():
         installation(localization_list[i])
         time.sleep(1)
         if if_na:
-            print("NA build used")
-            main_script(na_keys_list, na_names_list, localization_list[i])
+            print("'if_na = True' specified")
+            try:
+                main_script(na_keys_list, na_names_list, localization_list[i])
+            except (MatchError, ElementNotFoundError, base_wrapper.ElementNotEnabled):
+                print("Error: Element Not Found/Enabled! Please Re-run the script.")
+                logging.info("Error: Element Not Found/Enabled! Please Re-run the script.")
+                exit()
         else:
-            print("MAINT build used")
-            main_script(keys_list, names_list, localization_list[i])
+            print("'if_na = False' specified")
+            try:
+                main_script(keys_list, names_list, localization_list[i])
+            except (MatchError, ElementNotFoundError, base_wrapper.ElementNotEnabled):
+                print("Error: Element Not Found/Enabled! Please Re-run the script.")
+                logging.info("Error: Element Not Found/Enabled! Please Re-run the script.")
+                exit()
         time.sleep(1)
         uninstallation()
-    print("Operation is complete! See the log file 'C:\MediaCreationLog.log' for more information.")
-    print("\nCreate TRIAL US media manually!\n")
 
 
 if __name__ == '__main__':
     start()
+    print("Operation completed! See the log file 'C:\MediaCreationLog.log' for more information.")
+    print("\nCreate TRIAL US media manually!\n")
+    logging.info("Create TRIAL US media manually!\n")
